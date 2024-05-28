ceee8f5b22250968171746669ff22de53ff795f3
Marina Kononova <marinakononova@mail.ru>
2024-05-28 10:45:42 +0400

    extra Test

--- src/test/java/DateChangeTest.java
+++ src/test/java/DateChangeTest.java
@@ -54,7 +54,6 @@ public class DateChangeTest {
         $("[data-test-id='success-notification'] .notification__content")
                 .shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate))
                 .shouldBe(visible);
-
     }
 
     @Test
@@ -98,6 +97,26 @@ public class DateChangeTest {
         $(byText("Запланировать")).click();
         $("[data-test-id='success-notification'] .notification__content")
                 .shouldHave(exactText("Встреча уже успешно запланирована на " + firstMeetingDate))
+                .shouldBe(visible);
+    }
+
+    @Test
+    @DisplayName("Should Set Up Meeting If Name Has YO")
+    public void ShouldSetUpMeetingIfNameHasYO() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(firstMeetingDate);
+        $("[data-test-id=name] input").setValue("Королёв Пётр");
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("[data-test-id=agreement]").click();
+        $(byText("Запланировать")).click();
+        $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate))
                 .shouldBe(visible);
     }
 
