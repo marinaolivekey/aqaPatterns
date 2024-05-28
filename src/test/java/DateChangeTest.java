5fa170672c9c1941c0afb302280f6d9bccd43cd9
Marina Kononova <marinakononova@mail.ru>
2024-05-28 09:37:36 +0400

    Last Edition

--- src/test/java/DateChangeTest.java
+++ src/test/java/DateChangeTest.java
@@ -43,7 +43,7 @@ public class DateChangeTest {
         $("[data-test-id='success-notification'] .notification__content")
                 .shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate))
                 .shouldBe(visible);
-// choosing second date
+        // choosing second date
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(secondMeetingDate);
         $(byText("Запланировать")).click();
@@ -121,37 +121,3 @@ public class DateChangeTest {
     }
 }
 
-/*
-        $("[data-test-id='city'] input").setValue("Пермь");
-        String planningDate = generateDate(4, "dd.MM.yyyy");
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id='date'] input").setValue(planningDate);
-        $("[data-test-id='name'] input").setValue("Перов-Водкин Коля");
-        $("[data-test-id='phone'] input").setValue("+79001112233");
-        $("[data-test-id='agreement']").click();
-        $("button.button").click();
-        $(".notification__content")
-                .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
-    }
-
-
-    @Test
-    public void shouldTestSuccessfullySevenDaysCityFromMenu() {
-        $("[data-test-id='city'] input").setValue("Пе");
-        $$(".menu-item__control").findBy(Condition.text("Пермь")).click();
-        String planningDate = generateDate(7, "dd.MM.yyyy");
-        $("[data-test-id='date'] input").click();
-        if (!generateDate(3, "MM").equals(generateDate(7, "MM"))) {
-            $(".calendar__arrow_direction_right").click();
-        }
-        $$("[data-day]").findBy(Condition.text(generateDate(7, "d"))).click();
-        $("[data-test-id='name'] input").setValue("Перов-Водкин Коля");
-        $("[data-test-id='phone'] input").setValue("+79001112233");
-        $("[data-test-id='agreement']").click();
-        $("button.button").click();
-        $(".notification__content")
-                .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
-    }
- */
